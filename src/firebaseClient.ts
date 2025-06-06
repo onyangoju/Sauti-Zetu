91b5e604962d7a39655a1658838e89d48f80a95a
Onyango Onyango <147000453+onyangoju@users.noreply.github.com>
2025-06-06 19:20:45 +0300

    Update firebaseClient.ts

--- src/firebaseClient.ts
+++ src/firebaseClient.ts
@@ -1,16 +1,17 @@
+
 // src/firebaseClient.ts
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB32cemHHJqq2dHKQGKuQlollu7atifXOo",
-  authDomain: "sauti-zetu.firebaseapp.com",
-  databaseURL: "https://sauti-zetu-default-rtdb.firebaseio.com",
-  projectId: "sauti-zetu",
-  storageBucket: "sauti-zetu.appspot.com",
-  messagingSenderId: "1006205617279",
-  appId: "1:1006205617279:web:9b583fd81e9a0ecbba6b4a",
-  measurementId: "G-HQ1KL9J096"
+  apiKey: "AIzaSyC-7vaLFk4M3q6aE_3UZRLsVKyWfHC--FQ",
+  authDomain: "sauti-zetu-edc54.firebaseapp.com",
+  databaseURL: "https://sauti-zetu-edc54-default-rtdb.firebaseio.com", 
+  projectId: "sauti-zetu-edc54",
+  storageBucket: "sauti-zetu-edc54.firebasestorage.app",
+  messagingSenderId: "334240604370",
+  appId: "1:334240604370:web:8a8e644c073f697caa0d0b",
+  measurementId: "G-J5QC66SKBD"
 };
 
 const app = initializeApp(firebaseConfig);
